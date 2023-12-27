c645700ef3fc47850c69bbc9b1c16c174ea91106
sudalai14 <717821t148@kce.ac.in>
2023-12-27 16:08:38 +0530

    ancy

--- social/Backend/router/user.js
+++ social/Backend/router/user.js
@@ -2,13 +2,13 @@
 const router = require("express").Router();
 const User = require("../Modals/User");
 const { body, validationResult } = require('express-validator');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 // const { verifyToken } = require("./verifytoken");
 // const Post = require("../Modals/Post");
 // const { generateOTP } = require("./Extra/mail");
 // const VerificationToken = require("../Modals/VerificationToken");
-// const JWTSEC = "#2@!@$ndja45883 r7##";
+const JWTSEC = "#2@!@$ndja45883 r7##";
 // const nodemailer = require('nodemailer');
 // const ResetToken = require("../Modals/ResetToken");
 // const crypto = require("crypto");
@@ -28,25 +28,64 @@ router.post("/create/user" ,
           if(user){
                     return res.status(200).json("Please login with correct password")
           };
-        //   const salt = await bcrypt.genSalt(10);
-        //   const secpass = await bcrypt.hash(req.body.password , salt)
+           const salt = await bcrypt.genSalt(10);
+          const secpass = await bcrypt.hash(req.body.password , salt)
 
           user = await User.create({
                     username:req.body.username,
                     email:req.body.email,
-                    password:req.body.password,
+                    password:secpass,
                     profile:req.body.profile,
                     phonenumber:req.body.phonenumber
           })
 
-
+           const accessToken=jwt.sign({
+           id:user._id,
+           username:user.username
+           } ,JWTSEC)
           await user.save();
-          res.status(200).json(user)
+          res.status(200).json({user, accessToken})
         } catch(error){
          return res.status(400).json("Internal error occured")
 
         }
     })
+//        Login
+// router.post("/login" ,
+//     body('email').isEmail(),
+//     body('password').isLength({ min: 6 }) ,
+//     async(req , res)=>{
+//         //   const error = validationResult(req);
+//         //   if(!error.isEmpty()){
+//         //             return res.status(400).json("some error occured")
+//         //   }
+
+//         //   try {
+//           const user = await User.findOne({email:req.body.email});
+//           if(!user){
+//                   return res.status(400).json("User doesn't found")  
+//           }
+
+//           const Comparepassword = await bcrypt.compare(req.body.password , user.password);
+//           if(!Comparepassword){
+//                     return res.status(400).json("Password error")
+//           }
+//           const accessToken = jwt.sign({
+//                     id:user._id,
+//                     username:user.username
+//           }, JWTSEC);
+//           const {password , ...other} = user._doc
+//           res.status(200).json({other , accessToken});
+                    
+// // } catch (error) {
+// //             res.status(500).json("Internal error occured")        
+// // }
+
+// })
+
+
+
+
     module.exports=router;
 
 //           const accessToken = jwt.sign({
